9db833777a64ef8b2dd0e5a1637053ec8a092790
Palantoc <planvdz@gmail.com>
2022-11-23 12:22:00 -0600

    Se modifico el index.html a .php, tambien se agrego codigo para que de manera dinamica se muestren las ofertas

--- BaseDeDatos.sql
+++ BaseDeDatos.sql
@@ -1,29 +1,40 @@
-CREATE TABLE aspirante (
-    id_aspirante CHAR(4) PRIMARY KEY,
+CREATE TABLE aspirantes (
+    CURP CHAR(18) NOT NULL PRIMARY KEY,
     nombre VARCHAR(20) NOT NULL,
     apellidos VARCHAR(20) NOT NULL,
     oficio VARCHAR(50) NOT NULL,
     telefono CHAR(10) NOT NULL,
     correo VARCHAR(30) NOT NULL
-)
+);
 
-CREATE TABLE empresa(
+CREATE TABLE empresas(
     nombre VARCHAR(50) NOT NULL,
     direccion VARCHAR(50) NOT NULL,
     telefono CHAR(10)NOT NULL,
     correo VARCHAR(50) NOT NULL PRIMARY KEY,
     contrasena VARCHAR(50)NOT NULL,
     giro VARCHAR(50) NOT NULL
-)
+);
 
-CREATE TABLE oferta (
+CREATE TABLE ofertas (
     id_oferta int(5) AUTO_INCREMENT PRIMARY KEY,
-    area VARHCAR(50) NOT NULL,
+    area VARCHAR(50) NOT NULL,
     nombreOferta VARCHAR(100)NOT NULL,
     fechaDePublicacion DATE NOT NULL,
     sueldo FLOAT(5) NOT NULL,
     correo VARCHAR(50) NOT NULL,
-    FOREIGN KEY(correo) REFERENCES empresa(correo),
-    id_aspirante CHAR(4),
-    FOREIGN KEY(id_aspirante) REFERENCES aspirante(id_aspirante)
-)
+    FOREIGN KEY(correo) REFERENCES empresas(correo)
+);
+
+CREATE TABLE postulaciones(
+    id_postulacion int(5) AUTO_INCREMENT PRIMARY KEY,
+    id_oferta int(5),
+    FOREIGN KEY(id_oferta) REFERENCES ofertas(id_oferta),
+    CURP CHAR(18) NOT NULL,
+    FOREIGN KEY(CURP) REFERENCES aspirantes(CURP)
+);
+
+INSERT INTO empresas(nombre,direccion,telefono,correo,contrasena,giro) VALUES("Carpinteria Don Pedro","Calle 2 Oriente","2381452386","pedrito@gmail.com","pedrito1234","comisionista");
+INSERT INTO ofertas(id_oferta,area,nombreOferta,fechaDePublicacion,sueldo,correo) VALUES (NULL,"Construccion","Carpintero con o sin experiencia","2022-05-03",4500,"pedrito@gmail.com");
+INSERT INTO ofertas(id_oferta,area,nombreOferta,fechaDePublicacion,sueldo,correo) VALUES (NULL,"Chofer","Chofer de camionea","2022-05-04",3500,"pedrito@gmail.com");
+INSERT INTO ofertas(id_oferta,area,nombreOferta,fechaDePublicacion,sueldo,correo) VALUES (NULL,"Principal","Mantenimiento a la area de corte","2022-05-05",2800,"pedrito@gmail.com");
--- index.php
+++ index.php
@@ -102,17 +102,26 @@
             <div class="container pt-5 pb-3">
                 <h1 class="display-4 text-uppercase text-center mb-5">Vacantes de Empleo</h1>
                 <div class="row">
-                    <div class="col-lg-4 col-md-6 mb-2">
-                        <div class="rent-item mb-4">
-                            <img class="img-fluid mb-4" src="Imagenes/icons8-carpintero-89.png" alt="">
-                            <h4 class="text-uppercase mb-4">Carpinteria</h4>
-                            <p>area</p>
-                            <h4>sueldo</h4>
-                            <p>correo</p>
-                            <p>fecha de publicacion</p>
-                            <a class="btn btn-primary px-3" href="">Participar</a>
-                        </div>
-                    </div>
+                    <?php 
+                        require "Conexion.php";
+                        /*invoca una instruccion de sql*/
+                        $result = mysqli_query($connection,'SELECT area,nombreOferta,fechaDePublicacion,sueldo,correo FROM ofertas');
+                        while($row = mysqli_fetch_array($result)) {
+                            printf('<div class="col-lg-4 col-md-6 mb-2">
+                            <div class="rent-item mb-4">
+                                <img class="img-fluid mb-4" src="Imagenes/icons8-carpintero-89.png" alt="">
+                                <h4 class="text-uppercase mb-4">Carpinteria</h4>
+                                <p>%s</p>
+                                <h4>%s</h4>
+                                <p>%s</p>
+                                <p>%s</p>
+                                <a class="btn btn-primary px-3" href="">Participar</a>
+                            </div>
+                        </div>', $row["area"], $row["nombreOferta"], $row["fechaDePublicacion"],$row["sueldo"],$row["correo"]);
+                        }
+                        mysqli_free_result($result);
+                        mysqli_close($connection);
+                    ?>
                     <div class="col-lg-4 col-md-6 mb-2">
                         <div class="rent-item mb-4">
                             <img class="img-fluid mb-4" src="Imagenes/cerrajero.png" alt="">
@@ -271,72 +280,6 @@
         </div>
         <!-- Contact End -->
 
-
-        <!-- Footer Start -->
-        <div class="container-fluid bg-secondary py-5 px-sm-3 px-md-5" style="margin-top: 90px;">
-            <div class="row pt-5">
-                <div class="col-lg-3 col-md-6 mb-5">
-                    <h4 class="text-uppercase text-light mb-4">ponerse en contacto</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt text-white mr-3"></i> Tehuacan, Puebla</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt text-white mr-3"></i> +52 2386929352</p>
-                    <p><i class="fa fa-envelope text-white mr-3"></i> EmpleaTehuacan@gmail.com</p>
-                    <h6 class="text-uppercase text-white py-2">Siguenos</h6>
-                    <div class="d-flex justify-content-start">
-                        <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
-                        <a class="btn btn-lg btn-dark btn-lg-square" href="#"><i class="fab fa-instagram"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-5">
-                    <h4 class="text-uppercase text-light mb-4">Usefull Links</h4>
-                    <div class="d-flex flex-column justify-content-start">
-                        <a class="text-body mb-2" href="#"><i class="fa fa-angle-right text-white mr-2"></i>Private Policy</a>
-                        <a class="text-body mb-2" href="#"><i class="fa fa-angle-right text-white mr-2"></i>Term & Conditions</a>
-                        <a class="text-body mb-2" href="#"><i class="fa fa-angle-right text-white mr-2"></i>New Member Registration</a>
-                        <a class="text-body mb-2" href="#"><i class="fa fa-angle-right text-white mr-2"></i>Affiliate Programme</a>
-                        <a class="text-body mb-2" href="#"><i class="fa fa-angle-right text-white mr-2"></i>Return & Refund</a>
-                        <a class="text-body" href="#"><i class="fa fa-angle-right text-white mr-2"></i>Help & FQAs</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-5">
-                    <h4 class="text-uppercase text-light mb-4">Car Gallery</h4>
-                    <div class="row mx-n1">
-                        <div class="col-4 px-1 mb-2">
-                            <a href=""><img class="w-100" src="img/gallery-1.jpg" alt=""></a>
-                        </div>
-                        <div class="col-4 px-1 mb-2">
-                            <a href=""><img class="w-100" src="img/gallery-2.jpg" alt=""></a>
-                        </div>
-                        <div class="col-4 px-1 mb-2">
-                            <a href=""><img class="w-100" src="img/gallery-3.jpg" alt=""></a>
-                        </div>
-                        <div class="col-4 px-1 mb-2">
-                            <a href=""><img class="w-100" src="img/gallery-4.jpg" alt=""></a>
-                        </div>
-                        <div class="col-4 px-1 mb-2">
-                            <a href=""><img class="w-100" src="img/gallery-5.jpg" alt=""></a>
-                        </div>
-                        <div class="col-4 px-1 mb-2">
-                            <a href=""><img class="w-100" src="img/gallery-6.jpg" alt=""></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-5">
-                    <h4 class="text-uppercase text-light mb-4">Newsletter</h4>
-                    <p class="mb-4">Volup amet magna clita tempor. Tempor sea eos vero ipsum. Lorem lorem sit sed elitr sed kasd et</p>
-                    <div class="w-100 mb-3">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-dark border-dark" style="padding: 25px;" placeholder="Your Email">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary text-uppercase px-3">Sign Up</button>
-                            </div>
-                        </div>
-                    </div>
-                    <i>Lorem sit sed elitr sed kasd et</i>
-                </div>
-            </div>
-        </div>
         <div class="container-fluid bg-dark py-4 px-sm-3 px-md-5">
             <p class="mb-2 text-center text-body">&copy; <a href="#">Your Site Name</a>. All Rights Reserved.</p>
             <p class="m-0 text-center text-body">Designed by <a href="https://htmlcodex.com">HTML Codex</a></p>
